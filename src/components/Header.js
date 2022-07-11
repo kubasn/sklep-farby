2e373d05be90b933b55d951a6afc27e29b12032f
kubasn <sosinkuba@gmail.com>
2022-07-11 13:50:38 +0200

    11.07

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,34 @@ import Logo from "../assets/logo.png";
 import { CgClose, CgMenuRight } from "react-icons/cg";
 import { navigation } from "../data";
 import NavBar from "./NavBar";
+import { useEffect } from "react";
 
 const Header = () => {
-  const [show, setShow] = useState(false);
-  const [mobileView, setMobileView] = useState(true);
+  const [bg, setBg] = useState(false);
+  const [mobileView, setMobileView] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setBg(true) : setBg(false);
+    });
+  }, []);
+
   return (
     <header
       className={`${
-        show ? "bg-stone-800 py-4 lg:py-6 " : "bg-none"
+        bg ? "bg-stone-800 py-4 lg:py-6 opacity-95  " : "bg-none "
       }  fixed w-full z-10 left-0 py-4 transition-all duration-200`}
     >
       <div className="container mx-auto ">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-around items-center">
           <a href="#">
             <img src={Logo} alt="" width="200px" draggable="false" />
           </a>
 
-          <div className="text-5xl text-white cursor-pointer md:hidden">
+          <div
+            onClick={() => setMobileView(!mobileView)}
+            className="text-5xl text-white cursor-pointer md:hidden"
+          >
             {mobileView ? <CgClose /> : <CgMenuRight />}
           </div>
           <nav className="hidden md:flex">
@@ -42,7 +53,7 @@ const Header = () => {
           <div
             className={`${
               mobileView ? "left-0" : "-left-full"
-            } md:hidden fixed bottom-0 w-full max-w-xs h-screen transition-all `}
+            } md:hidden fixed bottom-0 w-full max-w-xs h-screen transition-all duration-700 `}
           >
             <NavBar />
           </div>
